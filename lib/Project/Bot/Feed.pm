0afab7dd476af7808f2d373d52fa4e02668b3b34
Andreas Marienborg <andreas.marienborg@gmail.com>
2010-09-17 10:48:15 +0700

    Change Feed to both auto and manual fetching
    Also added the opertunity of passing extra info to the callback

--- lib/Project/Bot/Feed.pm
+++ lib/Project/Bot/Feed.pm
@@ -6,26 +6,29 @@ class Project::Bot::Feed {
     
     has 'url' => (is => 'ro', isa => 'Str', required => 1);
     has 'interval' => (is => 'ro', isa => 'Int', default => 30);
-    has 'on_fetch' => (is => 'ro', isa => 'CodeRef', required => 1);
+    has 'on_fetch' => (is => 'ro', isa => 'CodeRef', required => 0);
     has 'username' => (is => 'ro', isa => 'Str', predicate => 'has_username');
     has 'password' => (is => 'ro', isa => 'Str');
     has 'filter'   => (is => 'ro', isa => FilterList, coerce => 1);
     
     has 'conn' => (
         is => 'ro', isa => 'AnyEvent::Feed', lazy => 1, builder => '_build_conn',
-        handles => [qw/fetch/],
     );
     
     method _build_conn() {
         AnyEvent::Feed->new(
             url => $self->url,
-            interval => $self->interval,
-            on_fetch => sub { $self->_got_items(@_) },
+            ($self->interval ? (interval => $self->interval) : ()),
+            on_fetch => sub { $self->_got_items([@_]) },
             ( $self->has_username ? ( username => $self->username, password => $self->password) : ())
         );
     }
-    
-    method _got_items($reader, $new_entries, $feed, $error?) {
+    method fetch($msg) {
+        $self->conn->fetch(sub { $self->_got_items([@_], $msg) });
+    }
+#    method _got_items(ArrayRef[] [$reader, $new_entries, $feed, $error?], $extra? ) {
+    method _got_items(ArrayRef $args, $extra?) {
+        my ($reader, $new_entries, $feed, $error) = (@$args);
         if (defined $error) {
             warn "ERROR: $error\n";
             return;
@@ -37,7 +40,8 @@ class Project::Bot::Feed {
                 }
             }
         }
-        $self->on_fetch->($reader, $new_entries, $feed);
+        $self->on_fetch->($reader, $new_entries, $feed, $extra);
     }
+
 }
 1;
