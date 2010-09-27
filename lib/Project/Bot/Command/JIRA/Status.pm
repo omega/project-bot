d2ce3c4c3cb3c5f7a55587a08affda973a88f26a
Andreas Marienborg <andreas.marienborg@gmail.com>
2010-09-27 19:28:10 +0700

    Update JIRA::Status to use new Response class
    Also change template to utilize this new format.

--- lib/Project/Bot/Command/JIRA/Status.pm
+++ lib/Project/Bot/Command/JIRA/Status.pm
@@ -2,9 +2,21 @@ use MooseX::Declare;
 namespace Project::Bot;
 class ::Command::JIRA::Status with ::SimpleCommand {
     use Project::Bot::HTTP;
-    
+    use Project::Bot::Message;
+    use Project::Bot::Command;
+    use Project::Bot::Command::JIRA::Response;
+
     has 'url' => (is => 'ro', isa => 'Str', );
-    has 'http' => (is => 'ro', isa => 'Project::Bot::HTTP|HashRef', initializer => '_init_http', handles => [qw/fetch/] );
+    has 'http' => (
+        is => 'ro', 
+        isa => 'Project::Bot::HTTP|HashRef', 
+        initializer => '_init_http', 
+        handles => [qw/fetch/] 
+    );
+    has 'title' => (
+        is => 'ro',
+        isa => 'Str',
+    );
     
     method _init_http($args, $setter, $attr) {
         my $feed = Project::Bot::HTTP::XML->new(
@@ -21,14 +33,16 @@ class ::Command::JIRA::Status with ::SimpleCommand {
         # render via $msg->render('jira-status.tt'), 
         warn "in fetched\n";
         # now to make $xml into something remotely useful for the template.
-        # For now: Array of Hashes, yay :p
-        $msg->reply($msg->render('jira-status.tt', { xml => $self->rewire( $xml ) }));
-    }
-    
-    method rewire($xml) {
-        map {
-            warn "item: $item";
-        } $xml->findnodes('/rss/channel/item');
+
+        $msg->reply($msg->render('jira-status.tt', { 
+                    title => $self->title,
+                    xml => Project::Bot::Command::JIRA::Response->new(
+                        dom => $xml
+                    ),
+                }
+            )
+        );
     }
 }
+
 1;
